344a591131a50c877a42ceb425ad5e372bae769f
arifeensaeed99 <108173265+arifeensaeed99@users.noreply.github.com>
2024-05-03 06:28:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def main():
             f.write(svg_file.getvalue())
         
         # Convert SVG file to PNG file using ImageMagic
-        subprocess.run( 'convert' + str( svg_file.name ) +  ' out.png')
+        subprocess.run( ' convert ' + str( svg_file.name ) +  ' out.png ')
 
         img = os.path.join(".", "out.png")
 
