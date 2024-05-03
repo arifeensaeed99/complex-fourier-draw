5b922b0f54bf62ad97ed0def8f7bafb522a42fb6
arifeensaeed99 <108173265+arifeensaeed99@users.noreply.github.com>
2024-05-03 05:28:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def main():
         # Convert SVG file to PNG file using ImageMagic
         subprocess.call(["convert", f.name, "out.png"], shell = True)
 
-        def file_selector(folder_path='.'):
+        def file_selector(folder_path=temp_dir):
             filenames = os.listdir(folder_path)
             selected_filename = st.selectbox('Select a file', filenames)
             return os.path.join(folder_path, selected_filename)
