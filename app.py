401324732b8e6c7b7d3209fa381fcd5836b05339
arifeensaeed99 <108173265+arifeensaeed99@users.noreply.github.com>
2024-05-06 07:01:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ def main():
     
                     line.set_data(xdata, ydata)
                     
-                    fig.savefig(temp_dir.name + "/" + str(i) + '.png')
+                    fig.savefig(temp_dir.name + str(i) + '.png')
     
                     # print progress
 
@@ -268,7 +268,7 @@ def main():
 
                 for i in range(len(fourier)):
                         
-                        exec('a'+str(i)+'=Image.open("'+temp_dir.name+"/"+str(i)+'.png")')
+                        exec('a'+str(i)+'=Image.open("'+temp_dir.name+str(i)+'.png")')
                         images.append(eval('a'+str(i)))
 
                 # Save the GIF
@@ -285,12 +285,12 @@ def main():
                 st.caption("(right click to download the gif)")
 
             # show gif
-            st.image(temp_dir.name + "/" + 'output.gif')
+            st.image(temp_dir.name + 'output.gif')
             
             # removing temp files
             for i in range(len(fourier)):
                 
-                os.remove(temp_dir.name + "/" + str(i)+'.png')
+                os.remove(temp_dir.name + str(i)+'.png')
 
             # st.caption("If need be, use this tool to speed up your gif: https://onlinegiftools.com/make-gif-faster")
 
