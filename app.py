07da2970d249d29446bdd7a04492e7245f4b4ed9
arifeensaeed99 <108173265+arifeensaeed99@users.noreply.github.com>
2025-12-09 02:49:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import fast_tsp
 from sklearn.cluster import mean_shift
 from sklearn.metrics import pairwise_distances
 
-os.environ["SYSTEM_GLOBALIZATION_INVARIANT"] = "true"
+os.environ["System.Globalization.Invariant"] = "true"
 
 try:
     import aspose.words as aw
